7f0a1d5c2fc2df243fe7e03486a131f2e8a62d8a
Jason Blickhan <jblickhan11@gmail.com>
2021-09-13 12:11:00 -0500

    non-secure cookie sessions, domain name in build-client, ingress load balancer fix

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -2,10 +2,14 @@ import axios from 'axios';
 
 const buildClient = ({ req }) => {
   if (typeof window === 'undefined') {
+    const baseURL =
+      process.env.NODE_ENV === 'production'
+        ? 'http://www.jbticketz.xyz'
+        : 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local';
+
     // We are on the server
     return axios.create({
-      baseURL:
-        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      baseURL,
       headers: req.headers,
     });
   } else {
