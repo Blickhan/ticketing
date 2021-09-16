bd7fa26ca420e733205d5f4cab51a0f69cc5db21
Jason Blickhan <jblickhan11@gmail.com>
2021-09-16 10:28:25 -0500

    add comment with local ingress url for client

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 const buildClient = ({ req }) => {
   if (typeof window === 'undefined') {
     // We are on the server
+    // local dev baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local'
     return axios.create({
       baseURL: 'http://www.jbticketz.xyz/',
       headers: req.headers,
